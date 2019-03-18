4581c47cc9f91eaf82a3a38dc15c6dfb1f2306fc
Jakob Grönlund <jakgr008@student.liu.se>
2019-03-18 09:50:14 +0100

    sizing

--- controller.js
+++ controller.js
@@ -2,9 +2,9 @@ function Controller(canvas) {
 
   var program = tdl.programs.loadProgramFromScriptTags("clicked_vs", "clicked_fs")
 
-  /*var arrays = {
-  position: new tdl.primitives.AttribBuffer(3, positionData),
-  };
+  //var arrays = {
+  //position: new tdl.primitives.AttribBuffer(3, positionData),
+  //};
 
-  canvas.onmousedown = function(event) {click(event, gl, canvas, positionLoc); };*/
+  //canvas.onmousedown = function(event) {click(event, gl, canvas, positionLoc); };*/
 }
